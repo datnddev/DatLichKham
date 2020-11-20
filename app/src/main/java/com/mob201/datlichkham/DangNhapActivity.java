d543a35e3877168f706d25260efbd2f5534ffab3
hau1112 <trandinhhauac@gmail.com>
2020-11-20 07:49:15 +0700

    tao button login

--- app/src/main/java/com/mob201/datlichkham/DangNhapActivity.java
+++ app/src/main/java/com/mob201/datlichkham/DangNhapActivity.java
@@ -2,14 +2,25 @@ package com.mob201.datlichkham;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DangNhapActivity extends AppCompatActivity {
-
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
-
+        btnRegister = (Button) findViewById(R.id.btnDangKy);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DangNhapActivity.this, DangKiActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
